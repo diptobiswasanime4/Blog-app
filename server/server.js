56f7e3860d8011ca4c22bd1a4ead44f776653f0e
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-10-04 20:56:30 +0530

    feat - cors

--- server/server.js
+++ server/server.js
@@ -16,6 +16,12 @@ const PORT = 3000;
 
 const uploadMiddleware = multer({ dest: "uploads/" });
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.use(
   cors({
